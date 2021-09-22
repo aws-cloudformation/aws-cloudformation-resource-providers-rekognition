208737d11e0d42c9e2bde834e42f58a71e9caf1a
Kadir Yasar <yasarka@amazon.com>
2021-09-22 21:22:42 +0000

    Add back Configuration file, needed for local tests

---
+++ aws-rekognition-project/src/main/java/software/amazon/rekognition/project/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.rekognition.project;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-rekognition-project.json");
+    }
+}
